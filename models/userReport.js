506b8321b3e555416bf4e68afbdb97301b9f60b5
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-09-24 18:26:45 -0500

    Add user and police report mongoose models

---
+++ models/userReport.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// TODO: UPDATE MAP POINT MODEL
+
+const userReportSchema = new Schema(
+    {
+        reportKey: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        genre: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        incidentKind: {
+            type: String,
+            required: true
+        },
+        mapPoint: {
+            type: String,
+            required: true
+        },
+        timestamp: {
+            type: String,
+            required: true
+        },
+        isVictim: {
+            type: Boolean,
+            required: true
+        },
+        isReportedToPolice: {
+            type: Boolean,
+            required: true
+        },
+        policeReport: {
+            type: Schema.Types.ObjectId,
+            ref: 'PoliceReport'
+        }
+    }
+);
+
+module.exports = mongoose.model('UserReport', userReportSchema);
